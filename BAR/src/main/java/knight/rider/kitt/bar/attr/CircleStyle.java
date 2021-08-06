176454493d40f8803581de0a83e82ad5f9406ed7
MacMini <macmini@MacMinideMac-mini.local>
2021-08-06 18:26:58 +0800

    1.0.2.4

---
+++ BAR/src/main/java/knight/rider/kitt/bar/attr/CircleStyle.java
@@ -0,0 +1,16 @@
+package knight.rider.kitt.bar.attr;
+
+/**
+ * 圆形红色实心
+ * 圆形红色实心白边
+ * 椭圆红色实心
+ * 椭圆红色实心白边
+ * 红点
+ */
+public enum CircleStyle {
+    CIRCLE_RED_SOLID,
+    CIRCLE_RED_SOLID_WRITE_STOKE,
+    OVAL_RED_SOLID,
+    OVAL_RED_SOLID_WRITE_STOKE,
+    DOT
+}
