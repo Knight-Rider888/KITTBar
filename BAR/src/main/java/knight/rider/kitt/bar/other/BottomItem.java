1231131765f3899d21fe8fae659fe9f898890801
MacMini <macmini@MacMinideMac-mini.local>
2021-08-09 18:55:46 +0800

    1.0.2.22

--- BAR/src/main/java/knight/rider/kitt/bar/other/BottomItem.java
+++ BAR/src/main/java/knight/rider/kitt/bar/other/BottomItem.java
@@ -1,51 +1,50 @@
 package knight.rider.kitt.bar.other;
 
 import android.content.Context;
-import android.util.AttributeSet;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import com.airbnb.lottie.LottieAnimationView;
 
 import knight.rider.kitt.bar.R;
 
-public class BottomItem extends FrameLayout {
+public class BottomItem extends LinearLayout {
 
-    // tab容器
-    private LinearLayout mTab;
+
     // icon容器
     private FrameLayout mIconLayout;
     // badge
     private TextView mBadge;
     // 文字
     private TextView mWord;
+    // 图片
+    private LottieAnimationView mLottie;
 
-    public BottomItem(@NonNull Context context) {
-        this(context, null);
-    }
+    private int mUnSelectedPicRes;
+    private int mSelectedPicRes;
 
-    public BottomItem(@NonNull Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
+    public BottomItem(Context context) {
+        super(context);
 
-    public BottomItem(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        // 垂直布局，宽高由BottomBar进行控制了
+        setOrientation(VERTICAL);
 
-        LayoutInflater.from(getContext()).inflate(R.layout.kitt_bottom_item, this, true);
+        // 直接添加到根视图
+        LayoutInflater.from(getContext()).inflate(R.layout.kitt_tab_icon, this, true);
+        LayoutInflater.from(getContext()).inflate(R.layout.kitt_tab_text, this, true);
 
-        mTab = (LinearLayout) findViewById(R.id.kitt_bar_item);
         mIconLayout = (FrameLayout) findViewById(R.id.kitt_bar_icon_layout);
         mBadge = (TextView) findViewById(R.id.kitt_bar_item_badge);
         mWord = (TextView) findViewById(R.id.kitt_bar_item_word);
+
+        mLottie = (LottieAnimationView) findViewById(R.id.kitt_bar_item_lottie);
     }
 
-
-    public LinearLayout getTabGroup() {
-        return mTab;
-    }
 
     public FrameLayout getIconGroup() {
         return mIconLayout;
@@ -58,4 +57,73 @@ public class BottomItem extends FrameLayout {
     public TextView getWordView() {
         return mWord;
     }
+
+    // TODO 红点待完成
+    public void init(int unSelectedRes, int selectedRes, int textColorUnSelected, int textColorSelected, int textSize) {
+
+        if (textColorUnSelected == 0)
+            textColorUnSelected = Color.parseColor("#666666");
+
+        if (selectedRes == 0)
+            selectedRes = unSelectedRes;
+
+        if (textColorSelected == 0)
+            textColorSelected = textColorUnSelected;
+
+        mSelectedPicRes = selectedRes;
+        mUnSelectedPicRes = unSelectedRes;
+
+        int picRes = isSelected() ? selectedRes : unSelectedRes;
+
+        if (picRes == 0) {
+            mLottie.setVisibility(INVISIBLE);
+        } else {
+            String typeName = getResources().getResourceTypeName(picRes);
+            if ("raw".equalsIgnoreCase(typeName)) {
+                // 动画资源
+                mLottie.setAnimation(picRes);
+            } else {
+                // 静态资源
+                mLottie.setImageResource(picRes);
+            }
+        }
+
+        mWord.setTextColor(getStateListColor(textColorUnSelected, textColorSelected));
+        mWord.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+    }
+
+    public void changeIcon() {
+
+        int picRes = isSelected() ? mSelectedPicRes : mUnSelectedPicRes;
+
+        if (picRes == 0) {
+            mLottie.setVisibility(INVISIBLE);
+        } else {
+            String typeName = getResources().getResourceTypeName(picRes);
+            if ("raw".equalsIgnoreCase(typeName)) {
+                // 动画资源
+                mLottie.setAnimation(picRes);
+                mLottie.playAnimation();
+            } else {
+                // 静态资源
+                mLottie.setImageResource(picRes);
+            }
+        }
+    }
+
+    private static ColorStateList getStateListColor(int textColorUnSelected, int textColorSelected) {
+
+        int[] colors = new int[]{textColorUnSelected, textColorSelected, textColorUnSelected};
+
+        int[][] states = new int[3][];
+
+        states[0] = new int[]{-android.R.attr.state_selected};
+
+        states[1] = new int[]{android.R.attr.state_selected};
+
+        states[2] = new int[]{};
+
+        return new ColorStateList(states, colors);
+    }
+
 }
